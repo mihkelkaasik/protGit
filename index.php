067997be41549b8e413236271e1c0d7941347783
mihkelkaasik <mihkelk@ut.ee>
2016-04-11 16:08:31 +0300

    test

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="stiilid.css">
+	<link rel="stylesheet" href="stiilid2.css">
+</head>
+<body>
+<div class="grid">
+	<div class="kast">
+		<div class="bottom"></div>
+		<div class="right"></div>
+		<div class="top"></div>
+		<div class="left">
+			<div id="tridiv">
+				<div class="scene">
+					<div class="shape prism-1 pri-1">
+						<div class="face ft"></div>
+						<div class="face bk"></div>
+						<div class="face-wrapper rt">
+							<div class="face"></div>
+						</div>
+						<div class="face-wrapper lt">
+							<div class="face"></div>
+						</div>
+						<div class="face bm"></div>
+					</div>
+				</div>
+			
+		</div>
+		</div>
+	</div>
+
+</div>
+	<div id="kasst">
+
+	<div id="tridiv">
+				<div class="scene">
+					<div class="shape prism-1 pri-1">
+						<div class="face ft"></div>
+						<div class="face bk"></div>
+						<div class="face-wrapper rt">
+							<div class="face"></div>
+						</div>
+						<div class="face-wrapper lt">
+							<div class="face"></div>
+						</div>
+						<div class="face bm"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+</body>
+</html>
