2b07227397d7e79c31e6c8ffb98df7eea8ff3964
mihkelkaasik <mihkelk@ut.ee>
2016-04-22 12:04:54 +0300

    d
    d

--- content/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="well">
-	<div class="row">
-	
-		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3 pull-right">
-			<h6><b>Built by <span class="text-info">Mihkel Kaasik</span></b></h6>
-		</div>
-		
-	</div>
-</div>
